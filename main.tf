f7ccc8df8588fe585de03e4b1d0012c927896426
huan <huan@yopmail.com>
2023-06-09 20:08:38 +0700

    update 14

--- main.tf
+++ main.tf
@@ -1,34 +1,16 @@
 provider "aws" {
-  region = var.aws_region
+  region = "us-east-1"  # Replace with your desired region
 }
 
-resource "aws_s3_bucket" "sample_bucket" {
-  bucket = "bucket-sample1000"
-}
-
-resource "aws_lambda_function" "sample_lambda" {
-  function_name = "sample_lambda_function"
-  handler      = "lambda_function.lambda_handler"
-  runtime      = "python3.8"
-  role         = aws_iam_role.sample_lambda_role.arn
-  source_code_hash = filebase64sha256("lambda_function.py")
-
-  environment {
-    variables = {
-      BUCKET_NAME = aws_s3_bucket.sample_bucket.bucket
-      FILE_NAME   = "sample.csv"
-    }
-  }
-}
-
-resource "aws_iam_role" "sample_lambda_role" {
-  name = "sample_lambda_role"
+resource "aws_iam_role" "lambda_execution_role" {
+  name = "lambda_execution_role"
 
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
   "Statement": [
     {
+      "Sid": "",
       "Effect": "Allow",
       "Principal": {
         "Service": "lambda.amazonaws.com"
@@ -38,4 +20,61 @@ resource "aws_iam_role" "sample_lambda_role" {
   ]
 }
 EOF
+
+  # Attach the necessary policies to the IAM role (adjust as per your requirements)
+  policy {
+    name   = "lambda_policy"
+    policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:GetObject"
+      ],
+      "Resource": "arn:aws:s3:::bucket-sample1000/sample.csv"  # Replace with your S3 bucket and file ARN
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:PutObject"
+      ],
+      "Resource": "arn:aws:s3:::bucket-sample1000/output/*"  # Replace with your S3 bucket output folder ARN
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_lambda_function" "sample_lambda" {
+  function_name    = "sample_lambda_function"  # Replace with your desired Lambda function name
+  role             = aws_iam_role.lambda_execution_role.arn
+  handler          = "lambda_function.lambda_handler"
+  runtime          = "python3.8"
+  timeout          = 60
+  memory_size      = 128  # Adjust as per your requirements
+
+  filename         = "lambda_function.py"  # Replace with the actual filename if different
+
+  # Environment variables (if required)
+  environment {
+    variables = {
+      BUCKET_NAME = "bucket-sample1000"  # Replace with your S3 bucket name
+      FILE_NAME   = "sample.csv"  # Replace with your file name
+    }
+  }
+}
+
+output "lambda_function_name" {
+  value = aws_lambda_function.sample_lambda.function_name
 }
