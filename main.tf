910de02dbfca8b283f32cb241a0f3f48280ade4d
huan <huan@yopmail.com>
2023-06-09 20:26:01 +0700

    update 18

--- main.tf
+++ main.tf
@@ -2,27 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_iam_role" "lambda_execution_role" {
-  name = "lambda_execution_role"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "",
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
-
-}
-
 resource "aws_iam_policy" "lambda_policy" {
   name        = "lambda_policy"
   description = "Policy for Lambda function"
@@ -61,12 +40,12 @@ EOF
 
 resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
   policy_arn = aws_iam_policy.lambda_policy.arn
-  role       = aws_iam_role.lambda_execution_role.name
+  role       = "Clone_data_from_GCS_to_S3-role-v2xqo06p"
 }
 
 resource "aws_lambda_function" "sample_lambda" {
-  function_name    = "sample_lambda_function_ffffff"
-  role             = aws_iam_role.lambda_execution_role.arn
+  function_name    = "sample_lambda_function_ff"
+  role             = "Clone_data_from_GCS_to_S3-role-v2xqo06p"
   handler          = "lambda_function.lambda_handler"
   runtime          = "python3.8"
   timeout          = 60
