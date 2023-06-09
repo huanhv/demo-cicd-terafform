b9157a484efa5798708a0ba5a7ea0ed5e01f5da2
huan <huan@yopmail.com>
2023-06-09 19:58:01 +0700

    update 11

--- variables.tf
+++ variables.tf
@@ -1,3 +1,21 @@
+variable "bucket_name" {
+  description = "The name of the S3 bucket"
+  type        = string
+  default     = "bucket-sample1000"
+}
+
+variable "file_name" {
+  description = "The name of the CSV file"
+  type        = string
+  default     = "sample.csv"
+}
+
+variable "lambda_function_name" {
+  description = "The name of the Lambda function"
+  type        = string
+  default     = "sample_lambda_function"
+}
+
 variable "aws_region" {
   description = "The AWS region where the resources will be deployed"
   type        = string
